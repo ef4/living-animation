608bd5ed0c53c30ece1e6c60174b167ef9741792
Edward Faulkner <ef@alum.mit.edu>
2018-03-09 00:28:34 -0500

    improvements

--- app/components/definition-slide.js
+++ app/components/definition-slide.js
@@ -1,12 +1,14 @@
 import Component from '@ember/component';
 import move from 'ember-animated/motions/move';
 import scale from 'ember-animated/motions/scale';
+import { fadeOut } from 'ember-animated/motions/opacity';
 import { parallel } from 'ember-animated';
 import { slideTransitionDuration } from 'living-animation';
 
 export default Component.extend({
   duration: slideTransitionDuration,
-  transition: function * ({ sentSprites }) {
+  transition: function * ({ sentSprites, removedSprites }) {
     sentSprites.forEach(parallel(move, scale));
+    removedSprites.forEach(fadeOut);
   }
 });
--- app/components/moving-word.js
+++ app/components/moving-word.js
@@ -2,6 +2,7 @@ import Component from '@ember/component';
 import move from 'ember-animated/motions/move';
 import adjustCSS from 'ember-animated/motions/adjust-css';
 import adjustColor from 'ember-animated/motions/adjust-color';
+import compensateForScale from 'ember-animated/motions/compensate-for-scale';
 import { parallel } from 'ember-animated';
 import { slideTransitionDuration } from 'living-animation';
 
@@ -12,6 +13,7 @@ export default Component.extend({
     sentSprites.forEach(
       parallel(
         move,
+        compensateForScale,
         adjustCSS.property('font-size'),
         adjustCSS.property('letter-spacing'),
         adjustColor.property('color')
