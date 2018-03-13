7dad57bc46c5fff73975e014410ef1539dde3a55
Edward Faulkner <ef@alum.mit.edu>
2018-03-13 15:42:11 -0700

    experimental keyboard support

--- src/ui/routes/tutorial-24/-utils/drag.js
+++ src/ui/routes/tutorial-24/-utils/drag.js
@@ -14,6 +14,12 @@ class Drag extends Motion {
     // Drag instances, because of interruption)
     this.dragStartX = null;
     this.dragStartY = null;
+
+    // When moving by keyboard, this is the net number of steps in
+    // each direction we have already taken from the start of the
+    // whole activity.
+    this.xStep = null;
+    this.yStep = null;
   }
 
   interrupted(motions) {
@@ -28,74 +34,156 @@ class Drag extends Motion {
     if (this.prior) {
       this.dragStartX = this.prior.dragStartX;
       this.dragStartY = this.prior.dragStartY;
+      this.xStep = this.prior.xStep;
+      this.yStep = this.prior.yStep;
       initialTx = sprite.transform.tx - sprite.absoluteInitialBounds.left + this.dragStartX;
       initialTy = sprite.transform.ty - sprite.absoluteInitialBounds.top + this.dragStartY;
     } else {
       this.dragStartX = sprite.absoluteInitialBounds.left;
       this.dragStartY = sprite.absoluteInitialBounds.top;
+      this.xStep = 0;
+      this.yStep = 0;
       initialTx = sprite.transform.tx;
       initialTy = sprite.transform.ty;
     }
 
     // targets are all in absolute screen coordinates
-    let targets = this._findTargets();
-    let ownTarget = centerPoint(sprite.absoluteFinalBounds);
+    let targets = this.opts.others.map(s => makeTarget(s.absoluteFinalBounds, s));
+    let ownTarget = makeTarget(sprite.absoluteFinalBounds, sprite);
 
     sprite.applyStyles({
-      zIndex: 1
+      zIndex: 1,
+      outline: 'dashed red' // before and after animationg are running,
+                      // users can tab through elements and rely on
+                      // outline to see it. But while we're actually in
+                      // the middle of a mouse drag, it's distracted.
     });
 
     while (sprite.owner.value.dragState) {
       let dragState = sprite.owner.value.dragState;
+      if (dragState.usingKeyboard) {
+        sprite.element.focus();
+        let chosenTarget = ownTarget;
+        while (this.xStep > dragState.xStep) {
+          chosenTarget = chooseNextToLeft(chosenTarget, targets);
+          this.xStep -= 1;
+        }
+        while (this.xStep < dragState.xStep) {
+          chosenTarget = chooseNextToRight(chosenTarget, targets);
+          this.xStep += 1;
+        }
+        while (this.yStep > dragState.yStep) {
+          chosenTarget = chooseNextToUp(chosenTarget, targets);
+          this.yStep -= 1;
+        }
+        while (this.yStep < dragState.yStep) {
+          chosenTarget = chooseNextToDown(chosenTarget, targets);
+          this.yStep += 1;
+        }
+        if (chosenTarget !== ownTarget) {
+          this.opts.onCollision(chosenTarget.payload);
+        }
+      } else {
+        // these track relative motion since the drag started
+        let dx = dragState.latestPointerX - dragState.initialPointerX;
+        let dy = dragState.latestPointerY - dragState.initialPointerY;
 
-      // these track relative motion since the drag started
-      let dx = dragState.latestPointerX - dragState.initialPointerX;
-      let dy = dragState.latestPointerY - dragState.initialPointerY;
+        // adjust our transform to match the latest relative mouse motion
+        sprite.translate(
+          dx + initialTx - sprite.transform.tx,
+          dy + initialTy - sprite.transform.ty
+        );
 
-      // adjust our transform to match the latest relative mouse motion
-      sprite.translate(
-        dx + initialTx - sprite.transform.tx,
-        dy + initialTy - sprite.transform.ty
-      );
+        // now this is our own absolute center position
+        let x = dx + this.dragStartX + sprite.absoluteFinalBounds.width / 2;
+        let y = dy + this.dragStartY + sprite.absoluteFinalBounds.height / 2;
 
-      // now this is our own absolute center position
-      let x = dx + this.dragStartX + sprite.absoluteFinalBounds.width / 2;
-      let y = dy + this.dragStartY + sprite.absoluteFinalBounds.height / 2;
+        let ownDistance = (x - ownTarget.x) * (x - ownTarget.x) + (y - ownTarget.y) * (y - ownTarget.y);
+        let closerTarget = targets.find(target => {
+          let partialX = target.x - x;
+          let partialY = target.y - y;
+          let distance = partialX * partialX + partialY * partialY;
+          return distance < ownDistance;
+        });
 
-      let ownDistance = (x - ownTarget.x) * (x - ownTarget.x) + (y - ownTarget.y) * (y - ownTarget.y);
-      let closerTarget = targets.find(target => {
-        let partialX = target.x - x;
-        let partialY = target.y - y;
-        let distance = partialX * partialX + partialY * partialY;
-        return distance < ownDistance;
-      });
-
-      if (closerTarget) {
-        this.opts.onCollision(closerTarget.sprite);
+        if (closerTarget) {
+          this.opts.onCollision(closerTarget.payload);
+        }
       }
-
       yield rAF();
     }
 
   }
 
-  _findTargets() {
-    return this.opts.others.map(otherSprite => {
-      let center = centerPoint(otherSprite.absoluteFinalBounds);
-      return {
-        x: center.x,
-        y: center.y,
-        sprite: otherSprite
-      };
-    });
-  }
+}
+
+export function makeTarget(bounds, payload) {
+  return {
+    x: bounds.left + bounds.width / 2,
+    y: bounds.top + bounds.height / 2,
+    payload
+  };
+}
+
+export function chooseNextToLeft(chosenTarget, targets) {
+  let candidates = targets.filter(target => {
+    if (target.x >= chosenTarget.x) {
+      return false;
+    }
+    let limit = (chosenTarget.x - target.x) / 2;
+    return target.y <= chosenTarget.y + limit &&
+      target.y >= chosenTarget.y - limit;
+  });
+  return closest(chosenTarget, candidates) || chosenTarget;
+}
+
+export function chooseNextToRight(chosenTarget, targets) {
+  let candidates = targets.filter(target => {
+    if (target.x < chosenTarget.x) {
+      return false;
+    }
+    let limit = (chosenTarget.x - target.x) / -2;
+    return target.y <= chosenTarget.y + limit &&
+      target.y >= chosenTarget.y - limit;
+  });
+  return closest(chosenTarget, candidates) || chosenTarget;
+}
+
+export function chooseNextToUp(chosenTarget, targets) {
+  let candidates = targets.filter(target => {
+    if (target.y >= chosenTarget.y) {
+      return false;
+    }
+    let limit = (chosenTarget.y - target.y) / 2;
+    return target.x <= chosenTarget.x + limit &&
+      target.x >= chosenTarget.x - limit;
+  });
+  return closest(chosenTarget, candidates) || chosenTarget;
+}
+
+export function chooseNextToDown(chosenTarget, targets) {
+  let candidates = targets.filter(target => {
+    if (target.y < chosenTarget.y) {
+      return false;
+    }
+    let limit = (chosenTarget.y - target.y) / -2;
+    return target.x <= chosenTarget.x + limit &&
+      target.x >= chosenTarget.x - limit;
+  });
+  return closest(chosenTarget, candidates) || chosenTarget;
 }
 
 
+function closest(chosenTarget, candidates) {
+  let closest;
+  let closestDistance;
+  for (let candidate of candidates) {
+    let distance = (candidate.x - chosenTarget.x) * (candidate.x - chosenTarget.x) + (candidate.y - chosenTarget.y) * (candidate.y - chosenTarget.y);
+    if (closestDistance == null || distance < closestDistance ) {
+      closestDistance = distance;
+      closest = candidate;
+    }
+  }
+  return closest;
 
-function centerPoint(bounds) {
-  return {
-    x: bounds.left + bounds.width / 2,
-    y: bounds.top + bounds.height / 2
-  };
 }
--- src/ui/routes/tutorial-24/controller.js
+++ src/ui/routes/tutorial-24/controller.js
@@ -2,8 +2,19 @@ import Controller from '../tutorial-01/controller';
 import move from 'ember-animated/motions/move';
 import { sortBy } from 'lodash';
 import { computed } from '@ember/object';
-import { task, rAF } from 'ember-animated';
-import drag from './-utils/drag';
+import drag, {
+  chooseNextToLeft,
+  chooseNextToRight,
+  chooseNextToUp,
+  chooseNextToDown,
+  makeTarget
+} from './-utils/drag';
+
+const LEFT_ARROW = 37;
+const UP_ARROW = 38;
+const RIGHT_ARROW = 39;
+const DOWN_ARROW = 40;
+const SPACE = 32;
 
 export default Controller.extend({
 
@@ -11,37 +22,107 @@ export default Controller.extend({
     return sortBy(this.model.toArray(), m => m.sortPriorityWithDefault);
   }),
 
-  beginDragging(mascot, event) {
-    this.get('drag').perform(mascot, event);
+  activateKeyboardNav() {
+    document.querySelector('.mascots .mascot-card').focus();
   },
 
-  drag: task(function * (mascot, event) {
-    let dragState = {
-      initialPointerX: event.x,
-      initialPointerY: event.y,
-      latestPointerX: event.x,
-      latestPointerY: event.y
-    };
-    function stop() {
-      mascot.set('dragState', null);
+  handleKey(mascot, event) {
+    let activeMascot = this.model.find(mascot => mascot.dragState);
+
+    if (activeMascot) {
+      let xStep = 0;
+      let yStep = 0;
+      switch (event.keyCode) {
+      case RIGHT_ARROW:
+        xStep = 1;
+        break;
+      case LEFT_ARROW:
+        xStep = -1;
+        break;
+      case DOWN_ARROW:
+        yStep = 1;
+        break;
+      case UP_ARROW:
+        yStep = -1;
+        break;
+      case SPACE:
+        activeMascot.set('dragState', null);
+        event.stopPropagation();
+        return false;
+      }
+      if (xStep || yStep) {
+        activeMascot.dragState.xStep += xStep;
+        activeMascot.dragState.yStep += yStep;
+        event.stopPropagation();
+        return false;
+      }
+    } else {
+      let elements = [...document.querySelectorAll('.mascots .mascot-card')].filter(element => element !== event.target);
+      let targets = [...elements].map(element => makeTarget(element.getBoundingClientRect(), element));
+      let currentTarget = makeTarget(event.target.getBoundingClientRect(), event.target);
+      let nextTarget;
+
+      switch (event.keyCode) {
+      case RIGHT_ARROW:
+        nextTarget = chooseNextToRight(currentTarget, targets);
+        break;
+      case LEFT_ARROW:
+        nextTarget = chooseNextToLeft(currentTarget, targets);
+        break;
+      case DOWN_ARROW:
+        nextTarget = chooseNextToDown(currentTarget, targets);
+        break;
+      case UP_ARROW:
+        nextTarget = chooseNextToUp(currentTarget, targets);
+        break;
+      case SPACE:
+        this.beginDragging(mascot, event);
+        event.stopPropagation();
+        return false;
+      }
+      if (nextTarget) {
+        nextTarget.payload.focus();
+        event.stopPropagation();
+        return false;
+      }
     }
-    function update(event) {
+  },
+
+  beginDragging(mascot, event) {
+    let dragState;
+
+    function stopMouse() {
+      mascot.set('dragState', null);
+      window.removeEventListener('mouseup', stopMouse);
+      window.removeEventListener('mousemove', updateMouse);
+    }
+
+    function updateMouse(event) {
       dragState.latestPointerX = event.x;
       dragState.latestPointerY = event.y;
     }
-    window.addEventListener('mouseup', stop);
-    window.addEventListener('mousemove', update);
-    mascot.set('dragState', dragState);
-    try {
-      while (mascot.dragState) {
-        yield rAF();
-      }
-    } finally {
-      mascot.set('dragState', null);
-      window.removeEventListener('mouseup', stop);
-      window.addEventListener('mousemove', update);
+
+    if (event instanceof KeyboardEvent) {
+      // This is a keyboard-controlled "drag" instead of a real mouse
+      // drag.
+      dragState = {
+        usingKeyboard: true,
+        xStep: 0,
+        yStep: 0,
+      };
+    } else {
+      dragState = {
+        usingKeyboard: false,
+        initialPointerX: event.x,
+        initialPointerY: event.y,
+        latestPointerX: event.x,
+        latestPointerY: event.y
+      };
+      window.addEventListener('mouseup', stopMouse);
+      window.addEventListener('mousemove', updateMouse);
     }
-  }),
+    mascot.set('dragState', dragState);
+  },
 
   transition: function * ({ keptSprites }) {
     let activeSprite = keptSprites.find(sprite => sprite.owner.value.dragState);
@@ -62,3 +143,7 @@ export default Controller.extend({
   },
 
 });
+
+function positiveMod(q, d) {
+  return (( q % d) + d) % d;
+}
